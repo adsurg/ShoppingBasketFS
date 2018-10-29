37f53f5046cc12a711fd055e79a3703c18e9a51e
Adam Surgenor <adam@surgenor.me>
2018-10-29 21:15:40 +0000

    The expected outcome when the same product is added twice.

--- Tests.fs
+++ Tests.fs
@@ -7,8 +7,17 @@ type Product = {name: string; price: decimal; quantity: int}
 
 let emptyBasket : Product list = []
 
+let rationalizeBasket (basket: Product list) =
+    basket |> List.groupBy (fun product -> product.name)
+           |> List.map (fun (key, products) -> 
+                {
+                name = key; 
+                price = products.Head.price; 
+                quantity = products |> List.sumBy (fun product->product.quantity)})
+
 let add product (basket : Product list) =
     basket @ [product]
+        |> rationalizeBasket
 
 let twoDecimalPlaces(number:decimal) =
     Math.Round(number, 2)
@@ -34,4 +43,20 @@ let ``When a five products are added to an empty basket the total is correct`` (
     
     Assert.Equal(199.95m, total)
 
+[<Fact>]
+let ``When a product is added twice to an empty basket the total is correct`` () =
+    let basket = emptyBasket
+                    |> add {name="Dove Soap"; price=39.99m; quantity=5}
+                    |> add {name="Dove Soap"; price=39.99m; quantity=3}
+    let total = subTotal basket        
     
+    Assert.Equal(319.92m, total)
+
+[<Fact>]
+let ``When a product is added twice to an empty basket the basket contains one entry with a summed quantity`` () =
+    let basket = emptyBasket
+                    |> add {name="Dove Soap"; price=39.99m; quantity=5}
+                    |> add {name="Dove Soap"; price=39.99m; quantity=3}
+    
+    Assert.Equal(1, basket.Length)
+    Assert.Equal({name="Dove Soap"; price=39.99m; quantity=8}, basket.Head)
