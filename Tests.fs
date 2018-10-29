688d5ebc542db4b35549acea0f57f251ab667997
Adam Surgenor <adam@surgenor.me>
2018-10-29 20:38:26 +0000

    Get the rounding right
    Simplify the types while I'm there

--- Tests.fs
+++ Tests.fs
@@ -1,16 +1,22 @@
 module Tests
 
 open Xunit
+open System
 
 type Product = {name: string; price: decimal}
 
 let emptyBasket : Product list = []
 
-let add (product : Product) (basket : Product list): Product list =
+let add product (basket : Product list) =
     basket @ [product]
 
-let subTotal (basket : Product list) : decimal =
-    basket |> List.sumBy  (fun product -> product.price)
+let twoDecimalPlaces(number:decimal) =
+    Math.Round(number, 2)
+
+let subTotal (basket : Product list) =
+    basket 
+        |> List.sumBy (fun product -> product.price)
+        |> twoDecimalPlaces
 
 [<Fact>]
 let ``When a product is added to an empty basket the total matches the price of the product`` () =
